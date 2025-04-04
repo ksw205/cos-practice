4b7cad206c18294d3861944eaec222d2a9e3ad28
[ksw205] <[keke@gmail.com]>
2025-04-04 01:49:42 +0000

    all

---
+++ 5/addition.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int uadd_ok(unsigned a, unsigned b) 
+{
+    unsigned int max = 0xffffffff;
+    unsigned int diff = max - a;
+    return diff > b;
+}
+int tadd_ok(int a, int b)
+{
+    int t, overflow, ret;
+    t = a+b;
+
+    overflow = (a>0 && b>0 && t<0) || (a<0 && b<0 && t>0);
+    if (overflow == 1)
+        ret = 0;
+    else
+        ret =1;
+    return ret;
+}
+int main(int argc, char *argv[])
+{
+    int a, b;
+    unsigned x, y;
+    if (argc != 3){
+        cout << "you should" << endl;
+    return 0;
+    }
+    
+    a = strtol(argv[1], NULL, 16);
+    b = strtol(argv[2], NULL, 16);
+    x = (unsigned) a;
+    y = (unsigned) b;
+    cout << "unsigned" << endl;
+    if (uadd_ok(x, y))
+        cout << "No overflow" << endl;
+    
+    else
+        cout << "overflow" << endl;
+    
+
+    cout << "sign"<<endl;
+    if (tadd_ok(a,b))
+        cout << "no overflow" << endl;
+    
+    else
+        cout << "overflow" << endl;
+    
+    return 0;
+}
